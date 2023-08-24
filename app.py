e74b2ea7b42fc6e9b42188dd846eb47a0dc9e60a
Anastasiia <nastyashch1601@gmail.com>
2023-08-24 10:07:45 +0100

    Add automatic upload folder creation

--- app.py
+++ app.py
@@ -10,25 +10,28 @@ app = Flask(__name__)
 STATIC_FOLDER = "static"
 UPLOAD_FOLDER = "static/uploads/"
 
+
 model = tf.keras.models.load_model(STATIC_FOLDER + "/models/" + "rps.h5")
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/classify", methods=["POST"])
 def upload_file():
 
-    file = request.files['image']
+    file = request.files["image"]
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
     filename = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(filename)
 
     predicted_class, probability = classify(model, filename)
-    return render_template('result.html', predicted_class=predicted_class, probability=round((probability * 100), 2))
+    return render_template("result.html", predicted_class=predicted_class, probability=round((probability * 100), 2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
     app.run(debug=True)
